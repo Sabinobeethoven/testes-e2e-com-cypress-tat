4f5f47ed911a2f7c3b5c6767fcd7b99dd1a8c1d7
L S <lourimar.sabino@edge.ufal.br>
2023-09-20 09:59:56 -0300

    Criando projeto e2e com cypress

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://notes-serverless-app.com',
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+    chromeWebSecurity: false
+    //setupNodeEvents(on, config) {
+    //implement node event listeners here
+    //},
+  },
+})
